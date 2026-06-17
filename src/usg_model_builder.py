Fix negative infiltration calculation artifact

src/usg_model_builder.py
@@ -455,11 +455,20 @@ def run_simulation(ts1_path: str, config: dict):
                 
                 # Inflow in m3/s
                 q_in_m3s = flows_m3day / 86400.0
-                if len(q_in_m3s) != len(dSdt):
-                    q_in_m3s = np.resize(q_in_m3s, len(dSdt))
+                
+                # Because dSdt is the average rate of change over the interval [t, t+1],
+                # we need to use the average inflow over the same interval to compute infiltration.
+                avg_q_in_m3s = (q_in_m3s[:-1] + q_in_m3s[1:]) / 2.0
+                if len(avg_q_in_m3s) > 0:
+                    avg_q_in_m3s = np.concatenate([[avg_q_in_m3s[0]], avg_q_in_m3s])
+                else:
+                    avg_q_in_m3s = q_in_m3s
+
+                if len(avg_q_in_m3s) != len(dSdt):
+                    avg_q_in_m3s = np.resize(avg_q_in_m3s, len(dSdt))
                 
                 # Modflow's exact net subsurface flux (m3/s) at each timestep
-                qinf_ts = q_in_m3s - dSdt
+                qinf_ts = avg_q_in_m3s - dSdt
                 
                 # We can just pass this exact array to the mass balance router
                 infiltration_rating = qinf_ts